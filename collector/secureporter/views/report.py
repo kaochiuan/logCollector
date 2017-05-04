bc505972c7a985a6581cbb13c7c426a19ef296df
kaochiuan.teng <kaochiuan.Teng@zyxel.com.tw>
2017-05-04 14:33:45 +0800

    [ENHANCEMENT] set today start time to midnight

--- collector/secureporter/views/report.py
+++ collector/secureporter/views/report.py
@@ -55,10 +55,13 @@ def __datetime_parse(start, end):
     if start is not None:
         datetime_start = datetime.strptime(start, '%Y-%m-%d')
     else:
-        datetime_start = datetime.today()
+        datetime_start = set_to_midnight(datetime.today())
     if end is not None:
         datetime_end = datetime.strptime(end, '%Y-%m-%d')
     else:
         datetime_end = datetime_start + timedelta(days=1)
 
     return datetime_start, datetime_end
+
+def set_to_midnight(dt):
+    return dt.replace(hour=0, minute=0, second=0, microsecond=0)
